816e9bd343f673cd1e9792d63e2dca3f06e30e94
yaseropozo <145109769+yaseropozo@users.noreply.github.com>
2023-12-20 21:07:43 +0300

    Create subdomain.sh

---
+++ subdomain.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <domains_file> <swordlist>"
+    exit 1
+fi
+
+domains_file="$1"
+
+# Check if the file exists
+if [ ! -f "$domains_file" ]; then
+    echo "Error: File '$domains_file' not found."
+    exit 1
+fi
+
+while IFS= read -r domain || [[ -n "$domain" ]]; do
+    output_file="temp_psubs_$domain.txt"
+    echo "Running subdomains_collector.py for domain: $domain"
+    python3 subdomains_collector.py "$domain" >> "$output_file" 2>&1
+    echo "Output saved to: $output_file"
+done < "$domains_file"
+
+cat "temp_psubs_"* | grep -v @ > "psubs_$1.txt"
+
+cat "temp_psubs_"* | grep @ > "emails_psubs_$1.txt"
+
+python3 subdomain-bruteforcer.py "$domains_file" > "fakesubs_$1"
+
+cat "psubs_$1.txt" "fakesubs_$1" | sort | uniq > "allsubs_$1"
+
+python3 massdns.py "allsubs_$1" "$3" | tee -a "subs_ip_$1"
