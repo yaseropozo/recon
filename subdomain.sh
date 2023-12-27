0773f9a14c2e6c14629697b6552e085e4b0cd11f
yaseropozo <145109769+yaseropozo@users.noreply.github.com>
2023-12-27 18:40:35 +0300

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <org_name> <swordlist> <dns resolver>"
+    echo "Usage: $0 <org_name> <swordlist> <dns_resolver>"
     exit 1
 fi
 
-domains_file="results/$1/domains.txt"
+org_name="$1"
+wordlist="$2"
+dns_resolver="$3"
+results_dir="results/$org_name"
+domains_file="$results_dir/domains.txt"
+temp_psubs_file="$results_dir/temp_psubs.txt"
+psubs="$results_dir/psubs.txt"
+emails_psubs="$results_dir/emails_psubs.txt"
 
 # Check if the file exists
 if [ ! -f "$domains_file" ]; then
@@ -13,20 +20,28 @@ if [ ! -f "$domains_file" ]; then
     exit 1
 fi
 
+# Create results directory if it doesn't exist
+mkdir -p "$results_dir"
+
 # Read each domain from the file and run the Python script
 while IFS= read -r domain || [[ -n "$domain" ]]; do
-    output_file="temp_psubs.txt"
     echo "Running subdomains_collector.py for domain: $domain"
-    python3 subdomains_collector.py "$domain" >> results/$1/"$output_file" 2>&1
-    echo "Output saved to: $output_file"
+    python3 subdomains_collector.py "$domain" >> "$temp_psubs_file" 2>&1
+    echo "Output saved to: $temp_psubs_file"
 done < "$domains_file"
 
-cat results/$1/"$output_file" | grep -v @ > "psubs.txt"
+# Filter out lines not containing '@' from temp_psubs.txt
+grep -v '@' "$temp_psubs_file" > "$psubs"
 
-cat results/$1/"$output_file" | grep @ > "emails_psubs.txt"
+# Filter out lines containing '@' from temp_psubs.txt
+grep '@' "$temp_psubs_file" > "$emails_psubs"
 
-python3 subdomain-bruteforcer.py "$domains_file" "$2" | tr -d "['\",]" | tr ' ' '\n' > results/$1/fakesubs.txt
+# Run subdomain-bruteforcer.py and process the output
+python3 subdomain-bruteforcer.py "$domains_file" "$wordlist" | tr -d "['\",]" | tr ' ' '\n' > "$results_dir/fakesubs.txt"
 
-cat results/$1/psubs.txt results/$1/fakesubs.txt | sort | uniq > results/$1/allsubs.txt
+# Combine and sort unique subdomains
+cat "$psubs" "$results_dir/fakesubs.txt" | sort | uniq > "$results_dir/allsubs.txt"
 
-python3 massdns.py results/$1/allsubs.txt "$3" | tee -a results/$1/subs_ip.txt
+# Run massdns.py and append results to subs_ip.txt
+python3 massdns.py "$results_dir/allsubs.txt" "$dns_resolver" | tee -a "$results_dir/subs_ip.txt"
+ 
