b15371eefce1c5f3a73fd39a72b0af94c6736832
yaseropozo <145109769+yaseropozo@users.noreply.github.com>
2023-12-27 17:28:02 +0300

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <domains_file> <swordlist>"
+    echo "Usage: $0 <org_name> <swordlist> <dns resolver>"
     exit 1
 fi
 
-domains_file="$1"
+domains_file="results/$1/domains.txt"
 
 # Check if the file exists
 if [ ! -f "$domains_file" ]; then
@@ -15,20 +15,18 @@ fi
 
 # Read each domain from the file and run the Python script
 while IFS= read -r domain || [[ -n "$domain" ]]; do
-    output_file="temp_psubs_$1"
+    output_file="temp_psubs.txt"
     echo "Running subdomains_collector.py for domain: $domain"
-    python3 subdomains_collector.py "$domain" >> "$output_file" 2>&1
+    python3 subdomains_collector.py "$domain" >> results/$1/"$output_file" 2>&1
     echo "Output saved to: $output_file"
 done < "$domains_file"
 
-cat "temp_psubs_"* | grep -v @ > "psubs_$1.txt"
+cat results/$1/"$output_file" | grep -v @ > "psubs.txt"
 
-cat "temp_psubs_"* | grep @ > "emails_psubs_$1.txt"
+cat results/$1/"$output_file" | grep @ > "emails_psubs.txt"
 
-python3 subdomain-bruteforcer.py "$domains_file" "$2" | tr -d "['\",]" | tr ' ' '\n' > "fakesubs_$1"
-wc "fakesubs_$1"
-wc "psubs_$1.txt"
+python3 subdomain-bruteforcer.py "$domains_file" "$2" | tr -d "['\",]" | tr ' ' '\n' > results/$1/fakesubs.txt
 
-cat "psubs_$1.txt" "fakesubs_$1" | sort | uniq > "allsubs_$1"
+cat "psubs.txt" results/$1/fakesubs.txt | sort | uniq > results/$1/allsubs.txt
 
-python3 massdns.py "allsubs_$1" "$3" | tee -a "subs_ip_$1"
+python3 massdns.py results/$1/allsubs.txt "$3" | tee -a results/$1/subs_ip.txt
