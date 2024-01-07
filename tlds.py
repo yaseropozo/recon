ad4aab66c526cd265d3f2ba4f2ba7d973b2824da
yaseropozo <145109769+yaseropozo@users.noreply.github.com>
2024-01-07 23:13:30 +0300

    Update tlds.py

--- tlds.py
+++ tlds.py
@@ -1,13 +1,29 @@
 import tldextract
 import sys
 import requests
-import time
+
 import re
+
+def is_valid_subdomain(subdomain):
+    # Check if the subdomain contains at least one dot
+    if '.' not in subdomain:
+        return False
+
+    return True
 
 def extract_apex_domain(subdomain):
     ext = tldextract.TLDExtract()
     extracted = ext(subdomain)
     return f"{extracted.domain}.{extracted.suffix}"
+
+def filter_subdomains(subdomains):
+    filtered_subdomains = []
+
+    for subdomain in subdomains:
+        if is_valid_subdomain(subdomain) and not re.match(r'^\d+\.\d+\.\d+\.\d+$', subdomain):
+            filtered_subdomains.append(subdomain)
+
+    return filtered_subdomains
 
 def main():
     if len(sys.argv) < 2:
@@ -27,29 +43,32 @@ def main():
     with open("json.txt", "w") as output_file:
         output_file.write(str(response.content) + "\n")
 
-    counter = 0 
-    start_time = time.time()
-    common_names = set()  # Use a set to store unique apex domains
+
+
+    subdomains_set = set()  # Use a set to store unique subdomains
 
     if response.status_code == 200:
         data = response.json()
         for item in data:
             common_name = item['common_name']
-            counter += 1
+
             if ' ' not in common_name:
                 if common_name.startswith("*."):
                     common_name = common_name[2:]
-                apex_domain = extract_apex_domain(common_name)
-                common_names.add(apex_domain)
-        
+                subdomain = extract_apex_domain(common_name)
+                if is_valid_subdomain(subdomain) and not re.match(r'\d+\.\d+\.\d+\.\d+', subdomain):
+                    subdomains_set.add(subdomain)
+
     else:
         print("Failed to retrieve data. HTTP status code:", response.status_code)
 
-    print("counter is :      "+str(counter))
-    
+
+
+    filtered_subdomains = filter_subdomains(subdomains_set)
     # Extract and print unique apex domains
-    for apex_domain in common_names:
-        print(f"Apex Domain: {apex_domain}")
+    for apex_domain in filtered_subdomains:
+        print(apex_domain)
 
 if __name__ == "__main__":
     main()
+
