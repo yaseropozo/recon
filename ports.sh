5b6dbd8da9e7e922a28c6d2af34206f6dd92ba8c
yaseropozo <145109769+yaseropozo@users.noreply.github.com>
2024-02-08 12:16:26 +0300

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the correct number of command-line arguments is provided
-if [ "$#" -lt 3 ]; then
+if [ "$#" -lt 2 ]; then
     echo "Usage: $0 <org_name> <default/top/range/list> [ports]"
     exit 1
 fi
@@ -9,10 +9,18 @@ fi
 # Assign command-line arguments to variables
 org_name="$1"
 mode="$2"
-ports="$3"
+
+
+echo "$1"
+echo "$2"
+
+
 
 # Set the results directory
 results_dir="results/$org_name"
+
+
+
 
 # Check if the mode is "range" or "list" and ensure the [ports] argument is provided
 if [ "$mode" == "range" ] || [ "$mode" == "list" ]; then
@@ -20,10 +28,16 @@ if [ "$mode" == "range" ] || [ "$mode" == "list" ]; then
         echo "Error: Ports argument is required for mode '$mode'."
         exit 1
     fi
+    ports="$3"
+    echo "$3"
+    python3 "port-scanner.py" "$results_dir/subs_ip.txt" "$mode" "$ports" > "$results_dir/ports.txt"
+    cat "$results_dir/ports.txt"
+    awk -F':' '{print $1":"$2}' "$results_dir/ports.txt"  > "$results_dir/portsonly" && python3 live-subdomain.py "$results_dir/portsonly"                                                                                                                                  
+    exit 0
 fi
-
 # Run the port scanner script
-python3 "port-scanner.py" "$results_dir/subs_ip.txt" "$mode" "$ports" > "$results_dir/ports.txt"
-
-awk -F':' '{print $1":"$2}' "$results_dir/ports.txt"  > "$results_dir/portsonly" && python3 live-subdomain.py "$results_dir/portsonly"                                                                                                                    
-   
+python3 "port-scanner.py" "$results_dir/subs_ip.txt" "$mode" > "$results_dir/ports.txt"
+cat "$results_dir/ports.txt"
+awk -F':' '{print $1":"$2}' "$results_dir/ports.txt"  > "$results_dir/portsonly" && python3 live-subdomain.py "$results_dir/portsonly"                                                                                                                                      
+  
+  
