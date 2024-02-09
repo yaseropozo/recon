67eeaa0a476e35720d5fc4a9841594f78b35a672
yaseropozo <145109769+yaseropozo@users.noreply.github.com>
2024-02-09 08:50:06 +0300

    Update live-subdomain.py

--- live-subdomain.py
+++ live-subdomain.py
@@ -3,34 +3,38 @@ import asyncio
 import aiohttp
 import aiodns
 
-
 target_ports = []
 
 
-
-
 async def check_subdomain(subdomain):
-    url = f"http://{subdomain}"
-    if await is_valid(url):
-        print(f"http://{subdomain}")
+    url_http = f"http://{subdomain}"
+    await check_and_print(url_http)
 
-    # Check HTTPS
-    url = f"https://{subdomain}"
-    if await is_valid(url):
-        print(f"https://{subdomain}")
+    url_https = f"https://{subdomain}"
+    await check_and_print(url_https)
+
+
+async def check_and_print(url):
+    status_code, response_length = await is_valid(url)
+    if status_code:
+        print(f"{url},{status_code},{response_length}")
+
 
 async def is_valid(url):
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, timeout=5) as response:
-                #print(response.headers)
-                return response.status is not None
+                status_code = response.status
+                response_length = len(await response.read())
+                return status_code, response_length
     except (aiohttp.ClientError, asyncio.TimeoutError):
-        return False
+        return None, None
+
 
 async def main(subdomains):
     tasks = [check_subdomain(subdomain) for subdomain in subdomains]
     await asyncio.gather(*tasks)
+
 
 if __name__ == "__main__":
     subdomains_file = sys.argv[1]
@@ -39,3 +43,4 @@ if __name__ == "__main__":
         subdomains_list = [line.strip() for line in file]
 
     asyncio.run(main(subdomains_list))
+ 
